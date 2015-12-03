26b3e75e0559a605eea657b3982415e58dc653a1
hwen <hwenleung@gmail.com>
2015-12-03 18:19:29 +0800

    .

--- js/weather.js
+++ js/weather.js
@@ -6,13 +6,18 @@ weather.controller('locationController', function() {
 
     (function() {
         var where = document.querySelector('.where');
+        var count=0;
         EventUtil.addHandler(where, 'input', function() {
-            var reg = new RegExp('^' + location.where + '.*$', 'im');
-            var count=0;
+            var reg = new RegExp('^' + where.value + '.*$', 'im');
+            console.log("where?"+where.value);
             for (var i=0; i<citys.length; i++) {
                 if (count > 2) break;
                 if (reg.test(citys[i][0])) {
+                    console.log(citys[i][0]);
                     location.tips.push(citys[i][0]);
+                    var li = document.createElement('li');
+                    li.innerHTML  = citys[i][0];
+                    where.appendChild(li);
                     count++;
                 }
             }
