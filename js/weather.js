febcb77bf6be27343dd7782fc473372d574f67db
hwen <hwenleung@gmail.com>
2015-12-04 13:31:15 +0800

    .

--- js/weather.js
+++ js/weather.js
@@ -1,9 +1,25 @@
 var weather = angular.module('ssweather', []);
 
 
-weather.controller('locationController', function() {
-    var location = this;
-    location.tips = [];
+weather.controller('weatherController',["$scope","$http", function($scope, $http) {
+    $scope.tips = [];
+    $scope.city = '上海';
+    $scope.temperature = '12';
+    
+    $scope.getWeatherNow = function(city) {
+        var api = 'https://api.thinkpage.cn/v2/weather/now.json?language=zh-chs&unit=c&key=STUQDFMKSP&city=';
+        api += city;
+        $scope.city = city;
+        $http({
+            method: 'JSONP',
+            url: api
+        }).then(function success(response) {
+            console.log(response);
+            $scope.temperature = data.now.temperature;
+        }, function error(response) {
+            console.log('get weather now error:'+response);
+        });
+    };
 
     (function() {
         var where = document.querySelector('.where');
@@ -13,28 +29,29 @@ weather.controller('locationController', function() {
 
         EventUtil.addHandler(where, 'input', function() {
             var reg = new RegExp('^' + where.value + '.*$', 'im');
-            console.log("where?"+where.value);
-            location.tips = [];
+            //console.log("where?"+where.value);
+            $scope.tips = [];
             count = 0;
 
             for (var i=0; i<citys.length; i++) {
                 if (count > 2) break;
                 if (reg.test(citys[i][0])) {
-                    console.log(citys[i][0]);
-                    location.tips.push(citys[i][0]);
+                    //console.log(citys[i][0]);
+                    $scope.tips.push(citys[i][0]);
                     count++;
                 }
             }
             ul.innerHTML = '';
-            for (var i=0; i<location.tips.length; i++) {
-                //ul.innerHTML += '<li onclick="getWeather()">'+location.tips[i]+'</i>';
+
+            for (var i=0; i<$scope.tips.length; i++) {
                 var li = document.createElement('li');
-                li.innerHTML = location.tips[i];
+                li.innerHTML = $scope.tips[i];
                 EventUtil.addHandler(li, 'click', function(e) {
-                    console.log(e.target.innerHTML);
+                    //console.log(e.target.innerHTML);
+                    $scope.getWeatherNow(e.target.innerHTML);
                 });
                 ul.appendChild(li);
             }
         });
     })();
-});
+} ]);
